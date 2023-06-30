95b1fb5994e8774e354aac3aa2756763c09c4b46
Daniel Ryan <ryannobusiness@gmail.com>
2023-06-30 17:10:52 +0100

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FC = () => {
+  const [messages, setMessages] = useState<Array<string>>([]);
 
-export default App;
+  const handleSend = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const form = event.target as HTMLFormElement;
+    const input = form.querySelector('input') as HTMLInputElement;
+    setMessages([...messages, input.value]);
+    input.value = '';
+  };
+
+  return (
+    <div className="App h-screen bg-gray-200 flex flex-col">
+      <header className="text-white bg-blue-600 p-4 text-center">
+        <h1 className="text-4xl">ChatGPT Replica</h1>
+      </header>
+      <div className="flex-1 overflow-auto p-4">
+        {messages.map((message, index) => (
+          <p key={index} className="my-2 p-2 bg-white rounded shadow">
+            {message}
+          </p>
+        ))}
+      </div>
+      <form onSubmit={handleSend} className="m-2">
+        <input 
+          type
