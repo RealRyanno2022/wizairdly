a8a4a2bcf4870ebc0cc3a78d9913fc5fb4986042
Daniel Ryan <ryannobusiness@gmail.com>
2023-06-30 17:24:37 +0100

    putting in work

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,34 @@
+import axios from 'axios';
 import React, { useState } from 'react';
 import './App.css';
 
-const App: React.FC = () => {
-  const [messages, setMessages] = useState<Array<string>>([]);
+type Message = {
+  content: string;
+  from: 'user' | 'ai';
+};
 
-  const handleSend = (event: React.FormEvent<HTMLFormElement>) => {
+const App: React.FC = () => {
+  const [messages, setMessages] = useState<Message[]>([]);
+
+
+  const handleSend = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const form = event.target as HTMLFormElement;
     const input = form.querySelector('input') as HTMLInputElement;
-    setMessages([...messages, input.value]);
+    const userMessage = input.value;
+    setMessages([...messages, {content: userMessage, from: 'user'}]);
+    
+    try {
+        const response = await axios.post('http://localhost:5000/message', { message: userMessage });
+        const aiMessage = response.data.message;
+        setMessages([...messages, {content: userMessage, from: 'user'}, {content: aiMessage, from: 'ai'}]);
+      } catch (error) {
+          console.error(error);
+      }
+
     input.value = '';
-  };
+};
+
 
   return (
     <div className="App h-screen bg-gray-200 flex flex-col">
@@ -20,10 +38,19 @@ const App: React.FC = () => {
       <div className="flex-1 overflow-auto p-4">
         {messages.map((message, index) => (
           <p key={index} className="my-2 p-2 bg-white rounded shadow">
-            {message}
+            {message.content} ({message.from})
           </p>
         ))}
       </div>
       <form onSubmit={handleSend} className="m-2">
         <input 
-          type
+          type="text" 
+          className="w-full p-2 rounded shadow"
+          placeholder="Type your message here" 
+        />
+      </form>
+    </div>
+  );
+}
+
+export default App;
